dc6c754523cad91d8bffe7dcf111bbc0795465dd
Michael Gale <m.gale@warwick.ac.uk>
2020-09-29 00:17:02 +0100

    GREAT IMRPVOEMENT

--- StrategyChangingAgent.java
+++ StrategyChangingAgent.java
@@ -12,18 +12,6 @@ public class StrategyChangingAgent implements IAgent {
     public HandShape nextMove() {
         int n = this.random.nextInt(0,8);
 
-        if(n == 0) {
-            n = this.random.nextInt(0,3);
-
-            switch(n) {
-                case 0: this.agent = new AlwaysRockAgent(); break;
-                case 1: this.agent = new AlwaysPaperAgent(); break;
-                case 2: this.agent = new AlwaysScissorsAgent(); break;
-            }
-
-            System.out.println("Changed to: " + this.agent.getName());
-        }
-
         return this.agent.nextMove();
     }
 
